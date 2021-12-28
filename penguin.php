0353f84fba4f300e5df86cc6f5fdb6c4128c0d76
Tsyuay <83605971+Tsyuay@users.noreply.github.com>
2021-12-27 23:10:36 -0300

    Add game files.

---
+++ penguin.php
@@ -0,0 +1,42 @@
+<html><head>
+
+
+
+
+
+
+
+</head><body bgcolor="#0099CC"><center>
+<a href="http://www.waffleisland.net/banner.php">
+<img border="0" alt="Waffle Island" src="banner.png" width="900" height="100">
+</a>
+
+<title>Penguin Chat 3</title>
+<font face="Arial, Helvetica, sans-serif" size="2">
+<br>
+<br>
+<center>
+<h2><center>New Things Coming Soon!</center></h2>
+
+<table border="0" cellspacing="0" cellpadding="1" align="center">
+<tbody><tr><td width="100%" align="center" valign="middle" bgcolor="#000000">
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="640" height="480">
+<param name="quality" value="high">
+<param name="bgcolor" value="#FFFFFF">
+<param name="menu" value="false">
+<param name="movie" value="chat.swf?build=1">
+<embed src="chat.swf?build=1" quality="high" bgcolor="#FFFFFF" menu="false" width="640" height="480" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+</object>
+
+</td></tr>
+</tbody></table>
+<br><font face="Arial, Helvetica, sans-serif" size="2"><b><center><u>Current online users</u><br></center></b><b><center>Karen Beecher</center></b><br><br>
+<font face="Arial, Helvetica, sans-serif" size="2">
+<!-- Penguin Chat -->
+<img border="0" alt="" src="http://penguinchat.x10.mx/penguinchat.org/banner2.png" width="900" height="100">
+
+
+
+
+</font></font></center></font></center></body></html>
+<!-- Is this how you did it? sir The Chosen One Panda Fake? well good job, this one is pretty rare. But I will try the next time myself. -Karen Beecher -->
